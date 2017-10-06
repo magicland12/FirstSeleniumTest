c88435faf135c2a2a06e5d0dc4bbea8a0d7b672d
magicland12 <sw_02b@mail.ru>
2017-10-06 15:26:21 +0300

    upd1

--- Test/Test1.py
+++ Test/Test1.py
@@ -3,9 +3,9 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-# driver = webdriver.Firefox()
-# driver = webdriver.Ie()
 
+
+# or you can set driver in PATH
 
 driver.set_page_load_timeout(10)
 driver.get("https://google.com")
@@ -13,3 +13,7 @@ driver.find_element_by_name("q").send_keys("Automation step by step")
 driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(4)
 driver.quit()
+driver.quit()
+
+print("Test has been successfully passed")
+
